4be30127b7bee424f14ac47f2fc8242dd8874d55
Retinug <Retinug@gmail.com>
2020-06-13 12:53:11 +0300

    Operations and numbers

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -6,114 +6,266 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApp1
 {
-    delegate void CaclulatorUpdateOutput(Calculator sender, double value, int precision);
-    delegate void CaclulatorInernalError(Calculator sender, string message);
-    enum CalculatorOperation { Add, Sub, Mul, Div}
-    enum CalculatorTransformation { Negate, Percent, Sqr, Sqrt, Inverse }
+    delegate void CalculatorUpdateOutput(Calculator sender, double value, int precision);
+    delegate void CalculatorInternalError(Calculator sender, string message);
+
+    enum CalculatorUnOperations { Invert, Reciprocal, Backspace, Sqr, Sqrt }
+    enum CalculatorOperations { Add, Sub, Mul, Div, Eq, Percent }
+
+
     class Calculator
     {
         double? input = null;
         double? result = null;
 
-        CalculatorOperation? op = null;
+        int? fractionDigits = null;
 
-        bool hasPoint;
-        int fractionDigits = 0;
+        CalculatorOperations? op = null;
 
-        public event CaclulatorUpdateOutput DidUpdateValue;
-        public event CaclulatorInernalError InputError;
-        public event CaclulatorInernalError ComputationError;
-
+        public event CalculatorUpdateOutput didUpdateValue;
+        public event CalculatorInternalError InputError;
+        public event CalculatorInternalError ComputationError;
         public void AddDigit(int digit)
         {
-            if (hasPoint == false)
+            if (fractionDigits.HasValue == false)
             {
-                if (input.HasValue && Math.Log10(input.Value) > 9)
+                if (input.HasValue && Math.Log10(input.Value) > 10)
                 {
-                    InputError?.Invoke(this, "Value is too long for this calculator!");
+                    InputError?.Invoke(this, "Value is too long for this calculator");
                     return;
                 }
-
-                input = (input ?? 0) * 10 + digit;                
+                input = (input ?? 0) * 10 + digit;
             }
             else
             {
-                if (fractionDigits > 8)
-                {
-                    InputError?.Invoke(this, "Value is too long for this calculator!");
-                    return;
-                }
-                fractionDigits += 1;
-                input = (input ?? 0) + digit * Math.Pow(10, -fractionDigits);
+                fractionDigits++;
+                input = (input ?? 0) + digit * Math.Pow(10, -fractionDigits.Value);
+
             }
-            DidUpdateValue?.Invoke(this, input.Value, fractionDigits);
+            didUpdateValue?.Invoke(this, input.Value, fractionDigits ?? 0);
         }
 
         public void AddPoint()
         {
-            hasPoint = true;
+            if (!fractionDigits.HasValue)
+                fractionDigits = 0;
         }
 
-        public void TransformInput(CalculatorTransformation t)
+        public void ProcessUnOp(CalculatorUnOperations op)
         {
             input = input ?? 0;
 
-            switch (t)
+            switch (op)
             {
-                case CalculatorTransformation.Negate:
+                case CalculatorUnOperations.Invert:
                     input = -input;
                     break;
+
+                case CalculatorUnOperations.Reciprocal:
+                    if (input.Value == 0)
+                        ComputationError?.Invoke(this, "Division by Zero");
+                    else
+                        input = 1 / input;
+                    break;
+
+                case CalculatorUnOperations.Sqr:
+                    input = Math.Pow(input.Value, 2);
+                    break;
+
+                case CalculatorUnOperations.Sqrt:
+                    input = Math.Sqrt(input.Value);
+                    break;
+
+                case CalculatorUnOperations.Backspace:
+                    if (input < 10)
+                        input = 0;
+                    else
+                    {
+                        string line = input.Value.ToString().Remove(input.Value.ToString().Length - 1);
+                        input = Convert.ToDouble(line);
+                    }
+
+                    break;
             }
-            DidUpdateValue?.Invoke(this, input.Value, fractionDigits);
+            didUpdateValue?.Invoke(this, input.Value, fractionDigits ?? 0);
+
         }
 
-        public void AddOperation(CalculatorOperation op)
+        public void ProcessBinOp(CalculatorOperations op)
         {
-            if (this.op.HasValue)
+            if (this.op.HasValue && input.HasValue && result.HasValue)
+            {
                 Compute();
+                this.op = op;
+            }
+            else
+                this.op = op;
 
-            this.op = op;
             if (input.HasValue)
             {
                 result = input;
-                this.Clear();
+                Clear();
             }
         }
 
         public void Compute()
         {
-            switch(this.op)
+            switch (op)
             {
-                case CalculatorOperation.Add:
+                case CalculatorOperations.Add:
                     result = result + input;
-                    DidUpdateValue?.Invoke(this, result.Value, 0);
-                    ResetInput();
                     break;
-                case CalculatorOperation.Div:
-                    if (input.HasValue && input.Value != 0)
+
+                case CalculatorOperations.Sub:
+                    result = result - input;
+                    break;
+
+                case CalculatorOperations.Mul:
+                    result = result * input;
+                    break;
+
+                case CalculatorOperations.Div:
+                    if (input.HasValue && input.Value == 0)
                     {
-                        result = result / input;
-                        DidUpdateValue?.Invoke(this, result.Value, 0);
-                        ResetInput();
+                        ComputationError?.Invoke(this, "Division by Zero");
+                        return;
                     }
                     else
-                        ComputationError?.Invoke(this, "Division by Zero");
+                    {
+                        result = result / input;
+                    }
                     break;
 
+                case CalculatorOperations.Percent:
+                    result = result * input / 100;
+                    break;
             }
-        }
 
-        void ResetInput()
-        {
+            didUpdateValue?.Invoke(this, result.Value, 0);
             input = null;
-            fractionDigits = 0;
-            hasPoint = false;
         }
 
         public void Clear()
         {
-            ResetInput();
-            DidUpdateValue?.Invoke(this, 0, 0);
+            input = null;
+            fractionDigits = null;
+            didUpdateValue?.Invoke(this, 0, 0);
+        }
+
+        public void ClearAll()
+        {
+            Clear();
+            result = null;
         }
     }
+    //delegate void CaclulatorUpdateOutput(Calculator sender, double value, int precision);
+    //delegate void CaclulatorInernalError(Calculator sender, string message);
+    //enum CalculatorOperation { Add, Sub, Mul, Div}
+    //enum CalculatorTransformation { Negate, Percent, Sqr, Sqrt, Inverse }
+    //class Calculator
+    //{
+    //    double? input = null;
+    //    double? result = null;
+
+    //    CalculatorOperation? op = null;
+
+    //    bool hasPoint;
+    //    int fractionDigits = 0;
+
+    //    public event CaclulatorUpdateOutput DidUpdateValue;
+    //    public event CaclulatorInernalError InputError;
+    //    public event CaclulatorInernalError ComputationError;
+
+    //    public void AddDigit(int digit)
+    //    {
+    //        if (hasPoint == false)
+    //        {
+    //            if (input.HasValue && Math.Log10(input.Value) > 9)
+    //            {
+    //                InputError?.Invoke(this, "Value is too long for this calculator!");
+    //                return;
+    //            }
+
+    //            input = (input ?? 0) * 10 + digit;                
+    //        }
+    //        else
+    //        {
+    //            if (fractionDigits > 8)
+    //            {
+    //                InputError?.Invoke(this, "Value is too long for this calculator!");
+    //                return;
+    //            }
+    //            fractionDigits += 1;
+    //            input = (input ?? 0) + digit * Math.Pow(10, -fractionDigits);
+    //        }
+    //        DidUpdateValue?.Invoke(this, input.Value, fractionDigits);
+    //    }
+
+    //    public void AddPoint()
+    //    {
+    //        hasPoint = true;
+    //    }
+
+    //    public void TransformInput(CalculatorTransformation t)
+    //    {
+    //        input = input ?? 0;
+
+    //        switch (t)
+    //        {
+    //            case CalculatorTransformation.Negate:
+    //                input = -input;
+    //                break;
+    //        }
+    //        DidUpdateValue?.Invoke(this, input.Value, fractionDigits);
+    //    }
+
+    //    public void AddOperation(CalculatorOperation op)
+    //    {
+    //        if (this.op.HasValue)
+    //            Compute();
+
+    //        this.op = op;
+    //        if (input.HasValue)
+    //        {
+    //            result = input;
+    //            this.Clear();
+    //        }
+    //    }
+
+    //    public void Compute()
+    //    {
+    //        switch(this.op)
+    //        {
+    //            case CalculatorOperation.Add:
+    //                result = result + input;
+    //                DidUpdateValue?.Invoke(this, result.Value, 0);
+    //                ResetInput();
+    //                break;
+    //            case CalculatorOperation.Div:
+    //                if (input.HasValue && input.Value != 0)
+    //                {
+    //                    result = result / input;
+    //                    DidUpdateValue?.Invoke(this, result.Value, 0);
+    //                    ResetInput();
+    //                }
+    //                else
+    //                    ComputationError?.Invoke(this, "Division by Zero");
+    //                break;
+
+    //        }
+    //    }
+
+    //    void ResetInput()
+    //    {
+    //        input = null;
+    //        fractionDigits = 0;
+    //        hasPoint = false;
+    //    }
+
+    //    public void Clear()
+    //    {
+    //        ResetInput();
+    //        DidUpdateValue?.Invoke(this, 0, 0);
+    //    }
+    //}
 }
--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WindowsFormsApp1;
 namespace WpfApp3
 {
@@ -25,22 +13,106 @@ namespace WpfApp3
         {
             InitializeComponent();
             calc = new Calculator();
-            calc.DidUpdateValue += Calc_DidUpdateValue;
+            calc.didUpdateValue += CalculatorDidUpdateValue;
+            calc.InputError += CalculatorError;
+            calc.ComputationError += CalculatorError;
+            calc.Clear();
         }
 
-        private void Calc_DidUpdateValue(Calculator sender, double value, int precision)
+        private void CalculatorError(Calculator sender, string message)
         {
-            output.Text = value.ToString();
+            MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void CalculatorDidUpdateValue(Calculator sender, double value, int precision)
         {
-            var button = sender as Button;
+            if (precision > 0)
+                output.Text = string.Format("{0:F" + precision + "}", value);
+            else
+                output.Text = $"{value}";
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
             int digit = -1;
+
             if (int.TryParse(button.Content.ToString(), out digit))
             {
                 calc.AddDigit(digit);
             }
+            else
+            {
+                switch (button.Tag)
+                {
+                    case "Add":
+                        calc.ProcessBinOp(CalculatorOperations.Add);
+                        break;
+                    case "Sub":
+                        calc.ProcessBinOp(CalculatorOperations.Sub);
+                        break;
+                    case "Mul":
+                        calc.ProcessBinOp(CalculatorOperations.Mul);
+                        break;
+                    case "Div":
+                        calc.ProcessBinOp(CalculatorOperations.Div);
+                        break;
+                    case "Eq":
+                        calc.ProcessBinOp(CalculatorOperations.Eq);
+                        break;
+                    case "Invert":
+                        calc.ProcessUnOp(CalculatorUnOperations.Invert);
+                        break;
+                    case "Point":
+                        calc.AddPoint();
+                        break;
+                    case "Reciprocal":
+                        calc.ProcessUnOp(CalculatorUnOperations.Reciprocal);
+                        break;
+                    case "CE":
+                        calc.Clear();
+                        break;
+                    case "C":
+                        calc.ClearAll();
+                        break;
+                    case "Percent":
+                        calc.ProcessBinOp(CalculatorOperations.Percent);
+                        break;
+                    case "Sqr":
+                        calc.ProcessUnOp(CalculatorUnOperations.Sqr);
+                        break;
+                    case "Sqrt":
+                        calc.ProcessUnOp(CalculatorUnOperations.Sqrt);
+                        break;
+                    case "Backspace":
+                        calc.ProcessUnOp(CalculatorUnOperations.Backspace);
+                        break;
+                }
+            }
+
         }
+
+        //Calculator calc;
+        //public MainWindow()
+        //{
+        //    InitializeComponent();
+        //    calc = new Calculator();
+        //    calc.DidUpdateValue += Calc_DidUpdateValue;
+        //}
+
+        //private void Calc_DidUpdateValue(Calculator sender, double value, int precision)
+        //{
+        //    output.Text = value.ToString();
+        //}
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var button = sender as Button;
+        //    int digit = -1;
+        //    if (int.TryParse(button.Content.ToString(), out digit))
+        //    {
+        //        calc.AddDigit(digit);
+        //    }
+        //}
     }
 }
